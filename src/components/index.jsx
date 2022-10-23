bbbbc7f8b6c19ab70a3d672492c1ae287bbe827f
Ильхам Нафиков <ilham@MacBook-Air-Ilham.local>
2022-10-23 18:09:50 +0300

    first_commit

---
+++ src/components/index.jsx
@@ -0,0 +1,9 @@
+export {default as List} from './List';
+export {default as AddList} from './AddList';
+export {default as Tasks} from './Tasks';
+export {default as Badge} from './Badge';
+
+
+// import List from "./components/List";
+// import AddList from "./components/AddList";
+// import Tasks from "./components/Tasks";
